5871a357f3f9f133a30ec98f88fd627f52321687
irwan.nugraha@batmandiri.com <irwan.nugraha@batmandiri.com>
2020-07-14 04:26:31 +0700

    initial

---
+++ app/Http/Controllers/ClientValidationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class ClientValidationController extends Controller
+{
+    public function existMobileNumber(Request $request, $value) {
+        $result = User::where('mobile_number', $value)->exists();
+
+        return response()->json([
+            'message' => 'success',
+            'data' => !$result
+        ]);
+    }
+}
---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Validator;
+use Carbon\Carbon;
+
+class RegisterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        return response()->json([
+            'message'=>'success',
+            'data'=>User::all()
+        ], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $param = $request->input();
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required',
+            'mobile_number' => 'required',
+        ]);
+        if ($validator->fails()) {    
+            return response()->json($validator->messages(), 400);
+        }
+
+        if(empty($param['password'])) {
+            $param['password'] = "123qwe";
+        }
+        if(isset($param['date'], $param['month'], $param['year'])) {
+            $param['birth_date'] = Carbon::createFromFormat('d/m/Y',  $param['date'].'/'.$param['month'].'/'.$param['year']);
+        }
+
+        $user = User::create($param);
+
+        return response()->json([
+            'message'=>'success',
+            'data'=>$user
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
